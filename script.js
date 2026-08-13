df484d9a28739122599bfd0f0395a43899b2ab7d
emilinda1 <emily.carolaine.antunes@escola.pr.gov.br>
2026-08-13 08:21:00 -0300

    11

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
   {
     enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    alternativas:[
+      "Alternativa 1", 
+      "Alternativa 2"
+    ]
   },
   {
     enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
+    alternativas: [
+      "Alternativa 1",
+       "Alternativa 2"
+    ]
+  }
 ];
