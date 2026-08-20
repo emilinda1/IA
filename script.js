0cea81dc0a1ce95eddfc91b596dae75cd05a3e36
emilinda1 <emily.carolaine.antunes@escola.pr.gov.br>
2026-08-20 08:42:21 -0300

    1111

--- script.js
+++ script.js
@@ -6,43 +6,73 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado:
-      "Assim que saiu da escola voce se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-    alternativas: ["Isso e assustador!", "Isso e maravilhoso!"],
+    enunciado:"Assim que saiu da escola voce se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+    alternativas: [
+      {
+     texto: "Isso e assustador!",
+      afirmacao: "afirmacao"
+    },
+{
+     texto: "Isso e maravilhoso!",
+      afirmacao: "afirmacao"
+    },
+]
   },
   {
-    enunciado:
-      "Com a descoberta desta tecnologia, chamada Inteligencia Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequencia de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+    enunciado:"Com a descoberta desta tecnologia, chamada Inteligencia Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequencia de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
     alternativas: [
-      "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informacoes relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-      "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos proprios sobre o tema.",
-    ],
+    {
+     texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informacoes relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+      afirmacao: "afirmacao"
+    },
+ {
+     texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos proprios sobre o tema.",
+      afirmacao: "afirmacao"
+    }
+    ]
   },
   {
-    enunciado:
-      "Apos a elaboracao do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa tambem foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+    enunciado:"Apos a elaboracao do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa tambem foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
     alternativas: [
-      "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-      "Me preocupo com as pessoas que perderao seus empregos para maquinas e defendo a importancia de proteger os trabalhadores.",
-    ],
+      {
+     texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+      afirmacao: "afirmacao"
+    },
+       {
+     texto: "Me preocupo com as pessoas que perderao seus empregos para maquinas e defendo a importancia de proteger os trabalhadores.",
+      afirmacao: "afirmacao"
+    }
+    ]
   },
   {
-    enunciado:
-      "Ao final da discussao, voce precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+    enunciado:"Ao final da discussao, voce precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
     alternativas: [
-      "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-      "Criar uma imagem utilizando um gerador de imagem de IA.",
-    ],
+       {
+     texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+      afirmacao: "afirmacao"
+    },
+       {
+     texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+      afirmacao: "afirmacao"
+    }
+   ]
   },
   {
-    enunciado:
-      "Voce tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho esta um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+    enunciado:"Voce tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho esta um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
     alternativas: [
-      "Escrever comandos para o chat e uma forma de contribuir com o trabalho, por isso nao e um problema utilizar o texto inteiro.",
-      "O chat pode ser uma tecnologia muito avancada, mas e preciso manter a atencao pois toda maquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+      {
+     texto:"Escrever comandos para o chat e uma forma de contribuir com o trabalho, por isso nao e um problema utilizar o texto inteiro.",
+      afirmacao: "afirmacao"
+    },
+     {
+     texto: "O chat pode ser uma tecnologia muito avancada, mas e preciso manter a atencao pois toda maquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+      afirmacao: "afirmacao"
+    },
+  ]
+},   
+ 
+      isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
     ],
-  },
-];
 
 let atual = 0;
 let perguntaAtual;
